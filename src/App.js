6010fcb0eddebb1b2fc954018da7c4a1986eec54
Alina <allianpi@gmail.com>
2023-07-08 14:54:26 -0400

    retrive rows from db

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './Pages/Login/Login';
 import SignUp from './Pages/SignUp/SignUp';
 import Footer from './components/Footer/Footer';
 import Maitenance from './Pages/Maintanence/Maitenance';
+import UserAcc from './Pages/UserAcc/UserAcc';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
   <Route path='/login' element={<Login/>}/>
   <Route path='/sign-up' element={<SignUp/>}/>
   <Route path='/sign-up' element={<SignUp/>}/>
+  <Route path='/acc-test' element={<UserAcc/>}/>
   <Route path='/maitenance' element={<Maitenance/>}/>
 </Routes>
 <Footer/>
--- src/Pages/UserAcc/UserAcc.js
+++ src/Pages/UserAcc/UserAcc.js
@@ -1,21 +1,46 @@
-import "./Maitenance.scss"
+import "./UserAcc.scss"
 import { Link } from "react-router-dom";
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 
-const userAcc = () => {
+const UserAcc = () => {
 
+const [rowList, setRowList] = useState([])
+
+useEffect(()=>{
+    axios.get("http://localhost:8080/row-list")
+    .then((response) =>{
+        setRowList(response.data)
+    })
+    .catch(console.log("wooopsy"))
+},[])
+console.log(rowList);
+
+
+if (!rowList) {
+return (
+    <h1>Loading...</h1>
+) 
+}
 return (
     <div className="test" >
         
-        {/* <div className="mait__container">
-            <h3 className="mait__title">Sorry, this page is currently under constuction...</h3>
-            <img className="mait__img" src={truck}/>
+        <div className="mait__container">
+            <h3 className="mait__title">THIS IS BACKEND TEST PAGE</h3>
+            
+            <ul>
+               {rowList.map(item =>
+            <li className='inv-list__item' key={item.row_id} >
+              <img className="test__img" src={item.row_img}/>  
+            </li>
+             )}
+            </ul>
             <Link to={"/"}><button className="mait__btn">BACK TO MAIN</button></Link>
-          
-        </div> */}
+        </div>
         
     </div>
 );
 }
 
-export default userAcc;
+export default UserAcc;
