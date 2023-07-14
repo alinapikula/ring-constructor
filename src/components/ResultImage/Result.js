b6e387e512ae98788a029ece22ce1cbf0bd9f48c
Alina <allianpi@gmail.com>
2023-07-14 17:08:26 -0400

    refactor of resultImg component

--- src/components/ResultImage/Result.js
+++ src/components/ResultImage/Result.js
@@ -6,25 +6,31 @@ import Shank from "../../assets/rows-dnd/bottom.png"
 import axios from "axios";
 
 function Result({state}) {
- const [row1, row2, row3] = Object.values(state)
 
+const [gen, setGen] = useState(false)
+const [resImg, setResImg] = useState([])
+const [row1, row2, row3] = Object.values(state)
+let img1
+let img2
+let img3
 useEffect((res, err)=>{
+  if (row1.length === 0) return
   axios.get(`http://localhost:8080/pos1/${row1[0].row_id}`)
-  .then ((res)=>{console.log(res.data.img);})
+  .then ((res)=>{img1 = res.data.img})
 })
 useEffect((res, err)=>{
+  if (row2.length === 0) return
   axios.get(`http://localhost:8080/pos2/${row2[0].row_id}`)
-  .then ((res)=>{console.log(res.data.img);})
+  .then ((res)=>{console.log(img2 = res.data.img);})
 })
 useEffect((res, err)=>{
+  if (row3.length === 0) return
   axios.get(`http://localhost:8080/pos3/${row3[0].row_id}`)
-  .then ((res)=>{console.log(res.data.img);})
+  .then ((res)=>{console.log(img3 = res.data.img);})
 })
 
 
 
-  const [gen, setGen] = useState(false)
-  const [resImg, setResImg] = useState([])
 
 // const containingNumber = (str) => {
 //  return str.match(/\d/g);
@@ -35,9 +41,7 @@ const handleGenerate = (e) =>{
       row2.length === 0 ||
       row3.length === 0 )
     return
-let img1
-let img2
-let img3
+
 // const stateRow1 = containingNumber(state.row1[0].content)
 // const stateRow2 = containingNumber(state.row2[0].content)
 // const stateRow3 = containingNumber(state.row3[0].content)
@@ -66,26 +70,26 @@ setResImg([img1, img2, img3])
 
 }
 
-// if (gen){
+if (gen){
   
-//  return (
+ return (
 
-//     <div >
-//             <div className="result">
-//             <div className="result__container result__container--rslt"  >
-//               <img className="result__img result__img--pos1" src={resImg[0]}/>
-//               <img className="result__img result__img--pos2" src={resImg[1]}/>
-//               <img className="result__img result__img--pos3" src={resImg[2]}/>
-//               <img className="result__img result__img--shank" src={Shank}/> 
-//             </div>
-//             <Link className="result__btn--link" to={"/maitenance"}><button className="result__btn result__btn--save">SAVE</button></Link>
-//             <button className="result__btn result__btn--clear" onClick={()=>{setGen(false)}}>CLEAR</button> 
-//             </div>
+    <div >
+            <div className="result">
+            <div className="result__container result__container--rslt"  >
+              <img className="result__img result__img--pos1" src={resImg[0]}/>
+              <img className="result__img result__img--pos2" src={resImg[1]}/>
+              <img className="result__img result__img--pos3" src={resImg[2]}/>
+              <img className="result__img result__img--shank" src={Shank}/> 
+            </div>
+            <Link className="result__btn--link" to={"/maitenance"}><button className="result__btn result__btn--save">SAVE</button></Link>
+            <button className="result__btn result__btn--clear" onClick={()=>{setGen(false)}}>CLEAR</button> 
+            </div>
 
-//             <button className="result__btn result__btn--out" onClick={(e)=>{handleGenerate(e)}} >GENERATE</button>
-//     </div>
-//   );
-// }
+            <button className="result__btn result__btn--out" onClick={(e)=>{handleGenerate(e)}} >GENERATE</button>
+    </div>
+  );
+}
             
   return (
 
